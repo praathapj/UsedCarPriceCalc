8030913733caa5128376b1b1e5e578a8277522b6
Prathap J <127469615+praathapj@users.noreply.github.com>
2023-03-25 19:56:42 +0530

    V3

--- UsedCarPriceCalc_App.py
+++ UsedCarPriceCalc_App.py
@@ -36,7 +36,7 @@ carPrice_model = pickle.load(pickle_in)
 
 # app title
 st.title("Check your car's price")
-st.write("Instant online quote")
+st.write("Instant online quote for CAR models before year 2022")
 
 # Get data 
 
